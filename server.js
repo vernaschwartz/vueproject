aaa96bdb838d8c4eb8cc06dce8ffd08c4513bcb0
Schwartz <Verna.Schwartz@navient.com>
2020-10-16 11:06:19 -0400

    chapter 10 push

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
+const enforce = require('express-sslify');
+
+const app= express();
+
+//forces users to use the HTTPS version of site
+app.use(enforce.HTTPS({trustProtoHeader: true}));
+app.use(serveStatic( _dirname +'/dist'));
+//helps prevent router history issues
+app.use(history());
+
+//makes heroku to use this port
+app.listen(process.env.PORT || 5000);
+
+
+
